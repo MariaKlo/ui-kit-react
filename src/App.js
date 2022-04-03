a3e26dfe7eada6a1b540830bf59192341e805f60
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-04-03 19:24:41 +0300

    Initial commit
    Start creating ui-kit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <div className="buttons">
+          <h1 className="buttons__header">Buttons</h1>
+          <div className="buttons__wrapper">
+            <div className="buttons__wrapper_pink">
+              <button className="button button_pink">Button</button>
+              <button className="button button_pink">Button</button>
+            </div>
+            <div className="buttons__wrapper_black">
+              <button className="button button_black">Button</button>
+              <button className="button button_black">Button</button>
+            </div>
+            <div className="buttons__wrapper_grey">
+              <button className="button button_grey">Button</button>
+              <button className="button button_grey">Button</button>
+            </div>
+            <div className="buttons__wrapper_purple">
+              <button className="button button_purple">Button</button>
+              <button className="button button_purple">Button</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
