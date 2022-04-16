1de2bd8f5a5bcf969fb1ab591109a867d2481b6c
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-04-16 15:05:47 +0300

    Add radio buttons

--- src/components/radio-buttons/radio-buttons.js
+++ src/components/radio-buttons/radio-buttons.js
@@ -1,1 +1,16 @@
-/* Add js code */
+import React from "react";
+
+class RadioButton extends React.Component {
+    render() {
+        return (
+            <div>
+                <button type="radio" name="radio">Radio button 1</button>
+                <button type="radio" name="radio">Radio button 2</button>
+                <button type="radio" name="radio">Radio button 3</button>
+                <button type="radio" name="radio">Radio button 4</button>
+            </div>
+        );
+    }
+}
+
+export default RadioButton;
