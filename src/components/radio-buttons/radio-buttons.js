158f9a41f004475a45a10752c9b2ecd07819595a
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-04-19 18:42:27 +0300

    Add radio-buttons' class names

--- src/components/radio-buttons/radio-buttons.js
+++ src/components/radio-buttons/radio-buttons.js
@@ -4,10 +4,14 @@ class RadioButton extends React.Component {
     render() {
         return (
             <div>
-                <button type="radio" name="radio">Radio button 1</button>
-                <button type="radio" name="radio">Radio button 2</button>
-                <button type="radio" name="radio">Radio button 3</button>
-                <button type="radio" name="radio">Radio button 4</button>
+                <br/>
+                <h1>Radio buttons</h1>
+                <div className="radio-buttons__wrap">
+                    <button type="radio" name="radio">Radio button 1</button>
+                    <button type="radio" name="radio">Radio button 2</button>
+                    <button type="radio" name="radio">Radio button 3</button>
+                    <button type="radio" name="radio">Radio button 4</button>
+                </div>
             </div>
         );
     }
