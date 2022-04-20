824bfe8a2be171f535ac31802fcedd82deb7f4ad
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-04-20 22:04:13 +0300

    Style radio buttons

--- src/components/radio-buttons/radio-buttons.js
+++ src/components/radio-buttons/radio-buttons.js
@@ -1,16 +1,21 @@
 import React from "react";
+import './radio-buttons.css';
 
 class RadioButton extends React.Component {
     render() {
         return (
-            <div>
+            <div className="radio-buttons">
                 <br/>
                 <h1>Radio buttons</h1>
                 <div className="radio-buttons__wrap">
-                    <button type="radio" name="radio">Radio button 1</button>
-                    <button type="radio" name="radio">Radio button 2</button>
-                    <button type="radio" name="radio">Radio button 3</button>
-                    <button type="radio" name="radio">Radio button 4</button>
+                    <input type="radio" className="radio-button radio-button_black" name="radio" id="radio_first"/>
+                    <label for="radio_first">Radio Button 1</label>
+                    <input type="radio" className="radio-button radio-button_grey" name="radio" id="radio_second"/>
+                    <label for="radio_second">Radio Button 2</label>
+                    <input type="radio" className="radio-button radio-button_purple" name="radio" id="radio_third"/>
+                    <label for="radio_third">Radio Button 3</label>
+                    <input type="radio" className="radio-button radio-button_pink" name="radio" id="radio_fourth"/>
+                    <label for="radio_fourth">Radio Button 4</label>
                 </div>
             </div>
         );
