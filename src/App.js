d8da4187adf80e101979f32779c313f620e997aa
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-04-20 22:01:52 +0300

    Style App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Buttons from './components/buttons/buttons.js';
 import Inputs from './components/inputs/inputs.js';
+import RadioButton from './components/radio-buttons/radio-buttons.js';
 
 function App() {
   return (
     <div className="App">
       <div className="wrapper">
         <Buttons/>
-        <Inputs text="Please, enter your name and password"/>
+        <div className='inputs__wrap'>
+          <Inputs text="Please, enter your name and password"/>
+          <Inputs text="Please, enter your name and password"/>
+          <Inputs text="Please, enter your name and password"/>
+        </div>
+        <RadioButton/>
       </div>
     </div>
   );
