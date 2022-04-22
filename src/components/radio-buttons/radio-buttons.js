cab0f1c9860c8a868304c9926eaa0348f7a3a606
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2022-04-22 18:09:50 +0300

    Edit radio buttons

--- src/components/radio-buttons/radio-buttons.js
+++ src/components/radio-buttons/radio-buttons.js
@@ -8,14 +8,22 @@ class RadioButton extends React.Component {
                 <br/>
                 <h1>Radio buttons</h1>
                 <div className="radio-buttons__wrap">
-                    <input type="radio" className="radio-button radio-button_black" name="radio" id="radio_first"/>
-                    <label for="radio_first">Radio Button 1</label>
-                    <input type="radio" className="radio-button radio-button_grey" name="radio" id="radio_second"/>
-                    <label for="radio_second">Radio Button 2</label>
-                    <input type="radio" className="radio-button radio-button_purple" name="radio" id="radio_third"/>
-                    <label for="radio_third">Radio Button 3</label>
-                    <input type="radio" className="radio-button radio-button_pink" name="radio" id="radio_fourth"/>
-                    <label for="radio_fourth">Radio Button 4</label>
+                    <div className="radio-button__wrap">
+                        <input type="radio" className="radio-button radio-button_black" name="radio" id="radio_first"/>
+                        <label className="label" htmlFor="radio_first">Radio Button 1</label>
+                    </div>
+                    <div className="radio-button__wrap radio-button__wrap_grey">
+                        <input type="radio" className="radio-button radio-button_grey" name="radio" id="radio_second"/>
+                        <label className="label" htmlFor="radio_second">Radio Button 2</label>
+                    </div>
+                    <div className="radio-button__wrap radio-button__wrap_purple">
+                        <input type="radio" className="radio-button radio-button_purple" name="radio" id="radio_third"/>
+                        <label className="label" htmlFor="radio_third">Radio Button 3</label>
+                    </div>
+                    <div className="radio-button__wrap radio-button__wrap_pink">
+                        <input type="radio" className="radio-button radio-button_pink" name="radio" id="radio_fourth"/>
+                        <label className="label" htmlFor="radio_fourth">Radio Button 4</label>
+                    </div>
                 </div>
             </div>
         );
